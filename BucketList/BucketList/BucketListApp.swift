1404403c7ebf240254053b2ea67f265ff1688ead
Dave Spina <dspina79@gmail.com>
2021-01-18 19:58:04 -0500

    Project initialization

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Dave Spina on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
