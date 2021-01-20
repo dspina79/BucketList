50b2892b76a18d2916e9ca63de4e0d0785ed80ca
Dave Spina <dspina79@gmail.com>
2021-01-19 22:06:58 -0500

    Added pin annotation example for Washington DC

--- BucketList/BucketList/MapView.swift
+++ BucketList/BucketList/MapView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            print(mapView.centerCoordinate)
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            
+            let mkView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            mkView.canShowCallout = true
+            return mkView
+        }
+    }
+    
     func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "Washington, DC"
+        annotation.subtitle = "Capital of the United States"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 35.53, longitude: -77.2)
+        mapView.addAnnotation(annotation)
         return mapView
     }
     
     func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
         
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
 }
 
