1fcb9d7dbe6def5e277a7f8265a673960ce54382
Dave Spina <dspina79@gmail.com>
2021-01-23 23:01:31 -0500

    Migrated the map view components into their own view

---
+++ BucketList/BucketList/CompositeMapView.swift
@@ -0,0 +1,94 @@
+//
+//  CompositeMapView.swift
+//  BucketList
+//
+//  Created by Dave Spina on 1/23/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct CompositeMapView: View {
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [CodableMKPointAnnotation]()
+    @State private var selectedPlace: MKPointAnnotation?
+    @State private var showingDetails = false
+    @State private var showingEditScreen = false
+    
+    var body: some View {
+        MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingDetails, annotations: locations)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        Circle()
+            .fill(Color.blue)
+            .opacity(0.3)
+            .frame(width: 32, height: 32)
+        
+        VStack {
+            Spacer()
+            HStack{
+                Spacer()
+                Button(action: {
+                    let newLocation = CodableMKPointAnnotation()
+                    newLocation.title = "Example Location"
+                    newLocation.coordinate = self.centerCoordinate
+                    self.locations.append(newLocation)
+                    
+                    self.selectedPlace = newLocation
+                    self.showingEditScreen = true
+                    
+                }) {
+                    Image(systemName: "plus")
+                        .padding()
+                        .background(Color.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
+                }
+            }
+        }
+        .alert(isPresented: $showingDetails){
+            Alert(title: Text(selectedPlace?.title ?? "Unknown Place"), message: Text(selectedPlace?.subtitle ?? "Missing Details"), primaryButton: .default(Text("Ok")), secondaryButton: .default(Text("Edit")){
+                self.showingEditScreen = true
+            })
+        }
+        .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
+            if self.selectedPlace != nil {
+                EditView(pointMark: self.selectedPlace!)
+            }
+        }
+        .onAppear(perform: loadData)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func loadData() {
+        let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+        
+        do {
+            let data = try Data(contentsOf: filename)
+            locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
+        } catch {
+            print("Unable to load saved data.")
+        }
+    }
+    
+    func saveData() {
+        do {
+            let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+            let data = try JSONEncoder().encode(self.locations)
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+        } catch {
+            print("Unable to save data...")
+        }
+    }
+}
+
+struct CompositeMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompositeMapView()
+    }
+}
